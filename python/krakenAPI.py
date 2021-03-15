34acf530aef4dcb329d4901e996e5570e0956453
mangose10 <leonar98@knights.ucf.edu>
2021-03-15 19:20:20 -0400

    semi update

--- python/krakenAPI.py
+++
@@ -1,6 +0,0 @@
-import pymongo, dns
-
-mongoc = pymongo.MongoClient("mongodb+srv://crespi:Simple1234@sctdb.v1k99.mongodb.net/test")
-
-latest = mongoc.SCT.Test.find_one({}, sort=[('_id', pymongo.DESCENDING)])
-mongoc.SCT.Test.replace_one(latest, {"set":{'it':'worked'}})
